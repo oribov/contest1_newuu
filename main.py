3eb5b9d26f26b3dcd890934e7b53c20387199a15
oribov <161562800+oribov@users.noreply.github.com>
2025-02-13 22:29:58 +0500

    Update main.py
    hi

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #e.g.
+from task1 import kwargsAcceptFun
 kwargsAcceptFun(ism1="Asadbek", yosh2=25, joy3="Beshariq")
 kwargsAcceptFun()
-
+from task2 import typeBasedTransformer
 info = {
     'a': 12,
     'b': 3.5,
@@ -17,3 +18,10 @@ rezultat = typeBasedTransformer(info)
 print(rezultat)
 
 
+from task3 import func,funx
+if __name__ == "__main__":
+    func()
+    funx()
+    func()
+    funx()
+    func()
